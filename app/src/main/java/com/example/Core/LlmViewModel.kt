c11b8abba58394b66ebd2ace8908964e9ab2d6a2
Youssef El idrissi <josephmaryem@Youssefs-MacBook-Pro.local>
2024-04-25 09:58:01 +0100

    clean LLM class

--- app/src/main/java/com/example/Core/LlmViewModel.kt
+++ app/src/main/java/com/example/Core/LlmViewModel.kt
@@ -10,44 +10,52 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 
-
 var category: Category? = null
-
 class Artificial_intelligence_model() : ViewModel() {
 
-    val generativeMultiModal = GenerativeModel(
+    private val generativeMultiModal = GenerativeModel(
         modelName = "gemini-pro-vision",
         apiKey = "AIzaSyDrjE857ITiU4y2i1Do7KEz_50dzFITnbY"
     )
-    val generativeText = GenerativeModel(
+
+    private val generativeText = GenerativeModel(
         modelName = "gemini-pro",
         apiKey = "AIzaSyDrjE857ITiU4y2i1Do7KEz_50dzFITnbY"
     )
-    var _uiState: MutableStateFlow<SummarizeUiState> =  MutableStateFlow(SummarizeUiState.Initial)
-    var uiState: StateFlow<SummarizeUiState> = _uiState.asStateFlow()
+
+    private val _uiState = MutableStateFlow<SummarizeUiState>(SummarizeUiState.Initial)
+    val uiState: StateFlow<SummarizeUiState> = _uiState.asStateFlow()
 
     fun resetUiStateToInitial() {
         _uiState.value = SummarizeUiState.Initial
     }
 
-
-
-    fun summarize(inputText: String, IsGetShortMessage: Boolean) {
-
-        if (IsGetShortMessage) {
-            var Brief = "As Pepper robot, Based on the detailed response provided by Pepper: '[${inputText}]', please generate a concise summary that captures the essential points and do not change the speaker's form. Please respond in plain text without using any Markdown formatting or links."
-            GeneratingMessage(Brief, IsGetShortMessage)
+    fun summarize(inputText: String, isGetShortMessage: Boolean) {
+        if (isGetShortMessage) {
+            val briefPrompt = prepareBriefPrompt(inputText)
+            generateMessage(briefPrompt, true)
             return
         }
-        val _category = category
-        val categoryInfo = getCategoryInfo(_category as Category)
-        val prompt = "As Pepper robot, Based on the description of the [${category}] category, which includes [${categoryInfo}],please provide an answer to the following user query: [${inputText}].If the query does not align with the category description,please utilize creative reasoning to offer a smart and relevant response, Please respond in plain text without using any Markdown formatting or links."
-        _uiState.value = SummarizeUiState.Loading
 
-        GeneratingMessage(prompt, false)
+        val prompt = prepareFullPrompt(inputText)
+        _uiState.value = SummarizeUiState.Loading
+        generateMessage(prompt, false)
     }
-    fun GeneratingMessage(prompt: String, IsGetShortMessage: Boolean) {
-            viewModelScope.launch {
+
+    private fun prepareBriefPrompt(inputText: String): String =
+        "As Pepper robot, based on the detailed response provided by Pepper: '[$inputText]', " +
+                "please generate a concise summary that captures the essential points and do not change the speaker's form. " +
+                "Please respond in plain text without using any Markdown formatting or links."
+    private fun prepareFullPrompt(inputText: String): String {
+        val categoryInfo = getCategoryInfo(category as Category)
+        return "As Pepper robot, based on the description of the [$category] category, which includes [$categoryInfo]," +
+                "please provide an answer to the following user query: [$inputText]. " +
+                "If the query does not align with the category description, please utilize creative reasoning to offer a smart and relevant response. " +
+                "Please respond in plain text without using any Markdown formatting or links."
+    }
+
+    fun generateMessage(prompt: String, IsGetShortMessage: Boolean) {
+        viewModelScope.launch {
             try {
                 var fulltext = ""
                 (generativeText).let { model ->
@@ -58,7 +66,6 @@ class Artificial_intelligence_model() : ViewModel() {
                         _uiState.value = SummarizeUiState.Success(fulltext)
                     else {
                         _uiState.value = SummarizeUiState.Prompt(fulltext)
-
                     }
                 }
             } catch (e: Exception) {
@@ -70,5 +77,4 @@ class Artificial_intelligence_model() : ViewModel() {
         }
     }
 }
-
 
